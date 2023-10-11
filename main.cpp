7668cac55413e9827c35a54b838a24fe042c12a4
ryoichi watanabe <k023g1065@outlook.com>
2023-10-11 10:16:11 +0900

    HeroworldをHeroworld変更に変更

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::ScreenPrintf(0, 0, "Heroworld");
+		Novice::ScreenPrintf(0, 0, "Heroworld変更");
 		///
 		/// ↑描画処理ここまで
 		///
