c9fb6ecb935b4ce1d4be1599d5b72ad878f90261
ryoichi watanabe <k023g1065@outlook.com>
2023-10-18 10:21:11 +0900

    日付pleyerをマージ

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		Novice::ScreenPrintf(0, 0, "Heroworld変更");
 		Novice::ScreenPrintf(0, 0, "日付10月11日");
-
+		Novice::DrawBox(0, 0, 0, 0, 0.0f,  BLUE, kFillModeSolid);
 		///
 		/// ↑描画処理ここまで
 		///
